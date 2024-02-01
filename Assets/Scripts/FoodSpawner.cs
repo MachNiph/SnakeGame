9b95c0f35fc034836b7606901cd235d969b46e82
Nishan Phuyal <nishanphuyal111@gmail.com>
2024-02-01 20:57:41 +0545

    added movement and spawn

--- Assets/Scripts/FoodSpawner.cs
+++ Assets/Scripts/FoodSpawner.cs
@@ -1,22 +1,29 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodSpawner : MonoBehaviour
+namespace Scripts
 {
-    [SerializeField] private GameObject food;
-    [SerializeField] private GameObject spawnedFood;
-
-    void Start()
+    public class FoodSpawner : MonoBehaviour
     {
-        StartCoroutine(SpawnFood());    
-    }
+        [SerializeField]
+        private GameObject food;
 
-    IEnumerator SpawnFood()
-    {
-        yield return new WaitForSeconds(3);
-        Destroy(spawnedFood);
-        spawnedFood = Instantiate(food,new Vector2(Random.Range(-8,8), Random.Range(-5,5)),Quaternion.identity);
+        [SerializeField]
+        private GameObject spawnedFood;
 
+        private void Start()
+        {
+            StartCoroutine(SpawnCoroutine());
+        }
+
+        private IEnumerator SpawnCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(5);
+                Destroy(spawnedFood);
+                spawnedFood = Instantiate(food, new Vector2(Random.Range(-8, 9), Random.Range(-4, 5)), Quaternion.identity);
+            }
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-
     private Vector2 inputVector;
+    private Vector2 rotateVector;
+    private Vector2 backPosition;
     [SerializeField] Vector2 maxPositionLimit;
+    [SerializeField] GameObject body;
+    [SerializeField] GameObject newBody;
 
     [SerializeField] private float time;
     [SerializeField] private float maxTime;
 
-    
     void Start()
     {
         inputVector = Vector2.right;
     }
+
     void Update()
     {
         Move();
+        eatFood();
     }
 
     void Move()
@@ -46,23 +51,43 @@ public class Player : MonoBehaviour
         if (time <= 0 && canMove())
         {
             transform.position += (Vector3)inputVector;
+            float angle = Mathf.Atan2(inputVector.y, inputVector.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
             time = maxTime;
         }
-
     }
 
     private bool canMove()
     {
         bool canMove = ((transform.position.x < maxPositionLimit.x) &&
-            (transform.position.x > -maxPositionLimit.x) && 
+            (transform.position.x > -maxPositionLimit.x) &&
             (transform.position.y > -maxPositionLimit.y &&
-            transform.position.y < maxPositionLimit.y));
+             transform.position.y < maxPositionLimit.y));
 
         return canMove;
     }
 
     void eatFood()
     {
+        float castLength = 0.5f;
+        LayerMask foodLayer = LayerMask.GetMask("Food");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, inputVector, castLength, foodLayer);
 
+        if (hit.collider != null)
+        {
+            Destroy(hit.collider.gameObject);
+
+            if (newBody != null)
+            {
+                backPosition = (Vector2)newBody.transform.position + (-inputVector);
+            }
+            else
+            {
+                backPosition = (Vector2)transform.position + (-inputVector);
+            }
+
+            newBody = Instantiate(body, backPosition, Quaternion.identity);
+            newBody.transform.parent = transform;
+        }
     }
 }
